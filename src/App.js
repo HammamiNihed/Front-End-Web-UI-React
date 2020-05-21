acaa28ec5c9b4033cefcf220dad2f15fab9f76f3
Nihed Hammami <nihedddhammami123456789@gmail.com>
2020-05-21 03:10:21 +0100

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from'reactstrap';
-import Menu from './components/MenuComponent';
-import './App.css';
+import Main from './components/MainComponent';
 
-class App extends Component {
-  render(){
-  return (
-    <div >
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+
+class App extends Component { 
+
+    render() {
+      return (
+        <div className="App">
+          <Main />
         </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
-}
+      );
+    }
+  
 }
 
 export default App;
