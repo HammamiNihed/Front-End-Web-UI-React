67e3c266909f0076a0e980fb0008095e8cd416b7
Nihed Hammami <nihedddhammami123456789@gmail.com>
2020-05-27 19:06:14 +0100

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody,Modal, ModalHeader, ModalBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button , Row, Col, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -66,9 +66,28 @@ function RenderComments({comments, addComment, dishId}) {
         }
     }
 
-    const  DishDetail = (props) => {        
+    const  DishDetail = (props) => { 
         const dish = props.dish;
-        if (dish == null) {
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }       
+        
+        else if (dish == null) {
             return (
                 <div></div>
             );
